99f10b602ce3506155cd82536994bc053b559424
Dhruvsandhu1 <152141078+Dhruvsandhu1@users.noreply.github.com>
2024-09-04 19:11:05 +0530

    Update Janie.py

--- Janie.py
+++ Janie.py
@@ -27,7 +27,7 @@ import os
 # load_dotenv()
 
 #Getting the Hugging Face Token and GroqApiKey
-os.environ['HF_TOKEN']=st.secrets['HF_TOKEN']
+# os.environ['HF_TOKEN']=st.secrets['HF_TOKEN']
 groq_api_key=st.secrets['GROQ_API_KEY']
 # embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
